6c47d66fe55c4f8984c5d4d18ade47e1f89ba929
anderskambenders <a.zetsmanis@mail.ru>
2024-01-24 22:38:41 +0300

    feat: add fs copy, create, rename

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,21 @@
+import {fileURLToPath} from 'url';
+import {dirname, resolve} from 'path';
+import {cp} from 'fs/promises';
+
 const copy = async () => {
-    // Write your code here 
+    const src = resolve(dirname(fileURLToPath(import.meta.url)), 'files');
+    const copy = resolve(dirname(fileURLToPath(import.meta.url)), 'files_copy');
+    try {
+        await cp(src, copy, {
+            recursive: true,
+            force: false,
+            errorOnExist: true,
+        });
+    } catch (error) {
+    if (error.code === 'ERR_FS_CP_EEXIST') {
+        console.log('FS operation failed');
+    }
+    }
 };
 
 await copy();
