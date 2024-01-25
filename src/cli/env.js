d4a4da2fd671b9487c2bb67a808d19a32a3c3b39
anderskambenders <a.zetsmanis@mail.ru>
2024-01-25 17:48:54 +0300

    feat: add cli solution

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,8 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envVars = Object.keys(process.env).filter(item => item.startsWith('RSS_'));
+    envVars.map(item => {
+        console.log(`${item}=${process.env[item]}`)
+    })
 };
 
 parseEnv();
