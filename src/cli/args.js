6c941558eddecfd72ac57ea68cd04384d99d2429
anderskambenders <a.zetsmanis@mail.ru>
2025-04-26 22:15:35 +0300

    feat: add cli solution

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,9 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv;
+    args.map((item, index) => {
+        if (item.startsWith('--') && args[index + 1])
+            console.log(`${item.replace('--', '')} is ${args[index + 1]}`);
+    });
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,8 @@
 const parseEnv = () => {
-    // Write your code here 
+    const vars = Object.keys(process.env).filter(item => item.startsWith('RSS_'));
+    vars.map(item => {
+        console.log(`${item}=${process.env[item]}`)
+    })
 };
 
 parseEnv();
