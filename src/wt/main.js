db0854f97dc6f88b9c636c169889282b07967698
anderskambenders <a.zetsmanis@mail.ru>
2025-04-28 00:40:17 +0300

    feat: add worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,23 @@
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { cpus } from 'os';
+import { Worker } from 'worker_threads';
+
 const performCalculations = async () => {
-    // Write your code here
+    const dir = dirname(fileURLToPath(import.meta.url));
+    const coreNum = cpus().length;
+    const exeFile = path.join(dir, 'worker.js');
+    const calculations = [];
+    for (let i = 0; i < coreNum; i +=1) {
+        const calc = new Promise((resolve, reject) => {
+            const worker = new Worker(exeFile, { workerData: i + 10 });
+            worker.on('message', (result) => resolve({ status: 'resolved', data: result }))
+            worker.on('error', () => reject({ status: 'error', data: null }))
+        })
+        calculations.push(calc)
+    }
+    Promise.all(calculations).then((data) => console.log(data)).catch((err) => console.log(err));
+
 };
 
 await performCalculations();
