1644a3371a62bd9e78a80709ea460e5ba8cc02f8
anderskambenders <a.zetsmanis@mail.ru>
2025-04-27 10:47:44 +0300

    feat: add hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import { createHash } from "crypto";
+import { dirname, resolve } from "path";
+import { readFile } from "fs/promises";
+import { fileURLToPath } from "url";
 
+const calculateHash = async () => {
+    const dir = dirname(fileURLToPath(import.meta.url));
+    const filePath = resolve(dir, "files", "fileToCalculateHashFr.txt");
+    try {
+        const data = await readFile(filePath, 'utf-8');
+        const hash = createHash('sha256').update(data).digest('hex');
+        console.log(hash);
+    }
+    catch (err) {
+        console.log(`FS operation failed.\n${err.message}`);
+    }
+};
 await calculateHash();
